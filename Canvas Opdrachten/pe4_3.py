c8c487732a48e1aaadeed2fb9a8b48d51f29f8aa
Max Schuurman <max.schuurman@student.hu.nl>
2018-09-20 23:23:44 +0200

    Import Git

--- Canvas Opdrachten/pe4_3.py
+++ Canvas Opdrachten/pe4_3.py
@@ -2,4 +2,3 @@ uurloon = float(input('Wat verdien je per uur: '))
 uren = int(input('Hoeveel uur heb je gewerkt: '))
 totaal = str(uren) + ' uur werken levert ' + str(uren * uurloon) + ' Euro op'
 print(totaal)
-print test = 'Dit is een test.'
